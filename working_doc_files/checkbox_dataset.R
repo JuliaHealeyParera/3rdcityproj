d4705778c9b0999faad0dd131132bc9603875e8b
Amy Duan <ad585@duke.edu>
2024-07-09 11:13:29 -0400

    joined almost complete and cod datasets

--- working_doc_files/checkbox_dataset.R
+++ working_doc_files/checkbox_dataset.R
@@ -42,3 +42,54 @@ cod_agg <- agg_data |>
            ifelse(uninten_non_drug_inj > 0, 1, 0) +
            ifelse(pending > 0, 1, 0)) |>
   mutate(four_cod = ifelse(tot_cod >=4, "Yes", "No"))
+
+dcra_variables <- c("c_system_abbr", "c_ind_full_name", "c_ind_dob_year", "c_ind_gender", "c_ind_race", "c_ind_ethnicity", "c_ind_dod_ymd", "ind_tod", "ind_deathloc", "c_ind_fachoused", "c_ind_cod_type")
+
+data <- agg_data %>%  
+  mutate(c_ind_first = 
+           case_when(
+             c_ind_first == "N/A" ~ NA, 
+             TRUE ~ c_ind_first),
+         c_ind_last = 
+           case_when(
+             c_ind_last == "N/A" ~ NA,
+             TRUE ~ c_ind_last)
+  )
+
+dcra_dataset <- data %>% 
+  mutate(c_ind_full_name = ifelse(!is.na(c_ind_first) & !is.na(c_ind_last),
+                                  paste(c_ind_first, " ", c_ind_last), 
+                                  NA)) %>% 
+  select(all_of(dcra_variables))
+
+dcra_table_variable <- dcra_dataset %>% 
+  group_by(c_system_abbr) %>% 
+  summarize(across(everything(), ~ round(mean(!is.na(.)) * 100, 2))) %>% 
+  select(all_of(c(dcra_variables))) %>% 
+  rename(system = c_system_abbr) %>% 
+  pivot_longer(!system, names_to = "variable", values_to = "percentages") 
+
+dcra_table_almost_complete <- dcra_table_variable %>% 
+  mutate(present_var = 
+           case_when(
+             percentages > 90 ~ "Present", 
+             TRUE ~ "Missing"
+             
+           )
+  ) %>% 
+  group_by(system, present_var) %>%
+  count() %>% 
+  filter(present_var == "Present") %>% 
+  mutate(almost_complete = 
+           case_when(
+             n >= 7 ~ "Yes", 
+             TRUE ~ "No"
+           ) 
+  ) %>%
+  ungroup() %>% 
+  select(system, almost_complete) %>% 
+  rename(c_system_abbr = system)
+
+cod_agg_2 <- cod_agg %>% 
+  left_join(dcra_table_almost_complete, by = "c_system_abbr")
+
