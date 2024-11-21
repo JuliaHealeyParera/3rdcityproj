8b1c076fcc41ed0e6a686b54a2b3c71b98c20c85
Julia Healey-Parera <jh884@duke.edu>
2024-11-20 23:25:16 -0500

    Added png files for all choropleth plots, saved as singular images

--- working_doc_files/cod_working_docs/summary_cod_chart_rver.R
+++ working_doc_files/cod_working_docs/summary_cod_chart_rver.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(ggplot2)
+library(here)
 
 ##Save R vis ggplot object, modify different ways for different uses
 
@@ -24,12 +25,20 @@ sum_cod <- sum_cod |>
 
 base <- ggplot(sum_cod, aes(x = all_passing, y = label)) +
   geom_col(fill = "#a6bddb", width = .9, position = 'dodge') +
-  geom_text(aes(label = all_passing), hjust = -.4, size = 3.3) +
+  geom_text(aes(label = all_passing), hjust = 1.75, size = 4, color = "white") +
   scale_x_continuous(limits = c(0, 54), expand = c(.01,0))  +
   scale_y_discrete(expand = c(.058, .058)) +
   theme_minimal() +
-  theme(plot.title = element_text(size = 15)) 
+  theme(plot.title = element_text(size = 15)) +
+  labs(x = "States Reporting", y = "")
+base
 
-base +
-  labs(x = "States Reporting", y = "", title = "No cause of death is reported by all systems.") 
+base_title <- base +
+  labs(title = "No cause of death is reported by all systems.") 
+base_title
 
+path_wo_title <- here("working_doc_files", "dashboard_visualizations", "summary_cod_chart_wo_title.png")
+path_w_title <- here("working_doc_files", "dashboard_visualizations", "summary_cod_chart_w_title.png") 
+
+ggsave(path_wo_title, plot = base, width = 10, height = 8, dpi = 300)
+ggsave(path_w_title, plot = base_title, width = 10, height = 8, dpi = 300)
